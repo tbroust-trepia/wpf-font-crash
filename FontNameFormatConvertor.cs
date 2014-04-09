5bd370392059d9538aac437e3ff244134e726d82
Thomas Broust <tbroust-trepia@ventadis.fr>
2014-04-09 10:10:23 +0200

    Test project that demonstrates Niconne WPF crash

---
+++ FontNameFormatConvertor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace test_fonts
+{
+    public class FontNameFormatConvertor : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            try
+            {
+                if (value == null)
+                {
+                    return string.Empty;
+                }
+
+                return value.ToString().Trim().Replace("./#", "");
+            }
+            catch (Exception)
+            {
+                return string.Empty;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return new FontNameFormatConvertor();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace test_fonts
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            this.Loaded += new RoutedEventHandler(this.MainWindow_Loaded);
+        }
+
+        private ObservableCollection<FontFamily> GetFontList()
+        {
+            var fontList = new ObservableCollection<FontFamily>();
+            var fonts = Fonts.SystemFontFamilies;
+            foreach (var Font in fonts)
+            {
+                fontList.Add(Font);
+            }
+            return fontList;
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.ddlFontName.ItemsSource = this.GetFontList(); 
+        }
+    }
+}
